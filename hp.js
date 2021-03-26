8e1f73ed471ee978522dc902cf4dc505593e6d89
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2021-03-26 17:21:37 +0900

    fix img_path

--- hp.js
+++ hp.js
@@ -1,7 +1,7 @@
 let header = document.querySelector('#header')
 
 header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-                      <a class="navbar-brand" href="index.html"><img src="image/なごみ.png" alt="訪問看護ステーションなごみロゴ"></a>
+                      <a class="navbar-brand" href="index.html"><img src="https://github.com/webSiteDev-NH/nagomi/blob/main/image/%E3%81%AA%E3%81%94%E3%81%BF.png?raw=true" alt="訪問看護ステーションなごみロゴ"></a>
                       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                       </button>
