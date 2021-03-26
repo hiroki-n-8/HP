04e5da339ae24081d636494b34ee1fb53a6df958
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2021-03-26 17:01:35 +0900

    add employment

--- hp.js
+++ hp.js
@@ -1,5 +1,20 @@
-let btn = document.querySelector('#btn')
+let header = document.querySelector('#header')
 
-btn.addEventListener('click', ()=>{
-  document.body.style.backgroundColor = "black";
-});
+header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+                      <a class="navbar-brand" href="index.html"><img src="image/なごみ.png" alt="訪問看護ステーションなごみロゴ"></a>
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+
+                      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                          <li class="nav-item">
+                            <a class="nav-link" href="employment.html">採用情報<span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                          </li>
+                        </ul>
+                        </form>
+                      </div>
+                    </nav>`
