c79ebd9fbbc4a617add0832dc42a4a894c1c27ad
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2021-03-31 16:12:34 +0900

    add information

--- hp.js
+++ hp.js
@@ -9,10 +9,13 @@ header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
                       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                           <li class="nav-item">
-                            <a class="nav-link" href="employment.html">採用情報<span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="index.html">サービス案内<span class="sr-only">(current)</span></a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="information.html">ステーション案内<span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="employment.html">採用情報<span class="sr-only">(current)</span></a>
                           </li>
                         </ul>
                         </form>
