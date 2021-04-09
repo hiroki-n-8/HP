d9201aba475a89c48869e6ebc03cda6fc9f5e7af
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2021-04-09 11:16:30 +0900

    add privacy-policy

--- hp.js
+++ hp.js
@@ -1,7 +1,10 @@
 let header = document.querySelector('#header');
 
 header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-                      <a class="navbar-brand" href="index.html"><img src="https://github.com/webSiteDev-NH/nagomi/blob/main/image/%E3%81%AA%E3%81%94%E3%81%BF.png?raw=true" alt="訪問看護ステーションなごみロゴ"></a>
+                      <div class="navbar-brand">
+                        <a href="index.html"><img src="https://github.com/webSiteDev-NH/nagomi/blob/main/image/%E3%81%AA%E3%81%94%E3%81%BF.png?raw=true" alt="訪問看護ステーションなごみ"></a>
+                        <div class="text-center text-danger">２４時間３６５日対応</div>
+                      </div>
                       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                       </button>
