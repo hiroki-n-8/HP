20160d261a210219ce7579159c39da97c3f30a6d
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2021-04-18 11:56:53 +0900

    modify HP

--- hp.js
+++ hp.js
@@ -3,7 +3,7 @@ let header = document.querySelector('#header');
 header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
                       <div class="navbar-brand">
                         <a href="index.html"><img src="https://github.com/webSiteDev-NH/nagomi/blob/main/image/%E3%81%AA%E3%81%94%E3%81%BF.png?raw=true" alt="訪問看護ステーションなごみ"></a>
-                        <div class="text-center text-danger">２４時間３６５日対応</div>
+                        <div class="nav-info text-center text-danger">２４時間　３６５日　緊急対応</div>
                       </div>
                       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
